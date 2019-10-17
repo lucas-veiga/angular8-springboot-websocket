b1db4c6e0ecf07130723ce61f63b0398f34164e4
Lucas Veiga <lucasveiga09@gmail.com>
2019-10-17 00:05:56 -0300

    polishing the example i took from the internet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { WebSocketAPI } from './websocket.service';
+import * as Stomp from 'stompjs';
 
 @Component({
   selector: 'app-root',
@@ -9,26 +9,52 @@ import { WebSocketAPI } from './websocket.service';
 export class AppComponent implements OnInit {
   title = 'angular8-springboot-websocket';
 
-  webSocketAPI: WebSocketAPI;
-  greeting = 'Aqui';
+  response = 'Aqui estara a resposta apos a emissao do evento';
   name: string;
+
+  webSocketEndPoint = 'http://localhost:8080/ws';
+  stompClient;
+
   ngOnInit() {
-    this.webSocketAPI = new WebSocketAPI(new AppComponent());
   }
 
   connect() {
-    this.webSocketAPI._connect();
+    console.log('Initialize WebSocket Connection');
+    this.stompClient = Stomp.client('ws://localhost:8080/ws');
+    this.stompClient.connect({}, (frame) => {
+      this.stompClient.subscribe('/topic/location/1', (sdkEvent) => {
+        this.onMessageReceived(sdkEvent);
+      });
+    }, this.errorCallBack);
   }
 
   disconnect() {
-    this.webSocketAPI._disconnect();
+    if (this.stompClient !== null) {
+      this.stompClient.disconnect();
+    }
+    console.log('Disconnected');
   }
 
   sendMessage() {
-    this.webSocketAPI._send(this.name);
+    console.log('calling logout api via web socket');
+    navigator.geolocation.getCurrentPosition((res) => {
+      const coords = res.coords;
+      this.stompClient.send(
+        `/app/location/${coords.latitude}/${coords.longitude}/driverId/${1}`,
+        {},
+        {});
+    });
   }
 
-  handleMessage(message) {
-    this.greeting = message;
+  onMessageReceived(message) {
+    console.log('Message Received from Server :: ' + message);
+    this.response = JSON.parse(message.body);
+  }
+
+  errorCallBack(error) {
+    console.log('errorCallBack -> ' + error);
+    setTimeout(() => {
+      this.connect();
+    }, 5000);
   }
 }
